d5b0c51b8ff6f3e5fd2b8f82d9d67442adbff113
daichang01 <18181985920@163.com>
2024-02-29 09:06:48 +0800

    Initial commit

---
+++ binocular_correction.py
@@ -0,0 +1,77 @@
+import numpy as np
+import cv2
+
+def load_calibration_data(left_calibration_path, right_calibration_path, stereo_calibration_path):
+    # 加载单目校准数据
+    left_data = np.load(left_calibration_path)
+    right_data = np.load(right_calibration_path)
+    
+    # 加载立体校准数据
+    stereo_data = np.load(stereo_calibration_path)
+    
+    return left_data['mtx'], left_data['dist'], right_data['mtx'], right_data['dist'], stereo_data['R'], stereo_data['T']
+
+def stereo_rectify(mtx_left, dist_left, mtx_right, dist_right, R, T, image_size, alpha=1):
+    # 计算校正变换
+    R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv2.stereoRectify(
+        mtx_left, dist_left, mtx_right, dist_right, image_size, R, T, alpha)
+    return R1, R2, P1, P2, Q
+
+def init_undistort_rectify_map(mtx, dist, R, P, image_size):
+    # 计算畸变校正和立体校正的映射矩阵
+    mapx, mapy = cv2.initUndistortRectifyMap(mtx, dist, R, P, image_size, cv2.CV_32FC1)
+    return mapx, mapy
+
+def rectify_images(left_img_path, right_img_path, map1x, map1y, map2x, map2y):
+    # 读取图像
+    left_img = cv2.imread(left_img_path)
+    right_img = cv2.imread(right_img_path)
+    
+    # 应用映射进行畸变校正和立体校正
+    rectified_left = cv2.remap(left_img, map1x, map1y, cv2.INTER_LINEAR)
+    rectified_right = cv2.remap(right_img, map2x, map2y, cv2.INTER_LINEAR)
+    
+    return rectified_left, rectified_right
+
+def main():
+    left_calibration_path = 'calibration_data_left.npz'
+    right_calibration_path = 'calibration_data_right.npz'
+    stereo_calibration_path = 'stereocali.npz'
+    left_img_path = "testleft.png"
+    right_img_path = "testright.png"
+    image_size = (1242, 2208)
+    
+    # 加载校准数据
+    mtx_left, dist_left, mtx_right, dist_right, R, T = load_calibration_data(
+        left_calibration_path, right_calibration_path, stereo_calibration_path)
+    
+    # 计算立体校正参数
+    R1, R2, P1, P2, Q = stereo_rectify(mtx_left, dist_left, mtx_right, dist_right, R, T, image_size)
+    
+    # 初始化畸变校正映射
+    map1x, map1y = init_undistort_rectify_map(mtx_left, dist_left, R1, P1, image_size)
+    map2x, map2y = init_undistort_rectify_map(mtx_right, dist_right, R2, P2, image_size)
+    np.savez('calibration_maps.npz', map1x=map1x, map1y=map1y, map2x=map2x, map2y=map2y)
+    
+    # 校正图像
+    rectified_left, rectified_right = rectify_images(left_img_path, right_img_path, map1x, map1y, map2x, map2y)
+    
+    # 保存和显示校正后的图像
+    cv2.imwrite("rectified_left.png", rectified_left)
+    cv2.imwrite("rectified_right.png", rectified_right)
+
+    concat = cv2.hconcat([rectified_left, rectified_right])
+
+    i = 0
+    while (i < 1242):
+        cv2.line(concat, (0,i), (4894,i), (0, 255, 0))
+        i += 50
+    cv2.imwrite('rectified.png',concat)
+    cv2.imshow("rectified", concat)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    print("Q:",Q)
+
+if __name__ == "__main__":
+    main()
---
+++ depth_all.py
@@ -0,0 +1,105 @@
+import cv2 
+import numpy as np
+import open3d as o3d
+
+
+def load_maps(filename):
+    """加载立体校正映射数据。"""
+    data = np.load(filename)
+    return data['map1x'], data['map1y'], data['map2x'], data['map2y']
+
+img = cv2.imread("testminxing.png")
+img_left = img[0:1242, 0:2208]
+img_right = img[0:1242, 2208:4416]
+
+map1x, map1y, map2x, map2y = load_maps("calibration_maps.npz")  
+
+img_left_rectified = cv2.remap(img_left, map1x, map1y, cv2.INTER_LINEAR)
+img_right_rectified = cv2.remap(img_right, map2x, map2y,cv2.INTER_LINEAR)
+img_color = cv2.cvtColor(img_left_rectified, cv2.COLOR_BGR2RGB)
+print(img_color.shape)
+
+imgL = cv2.cvtColor(img_left_rectified, cv2.COLOR_BGR2GRAY)
+imgR = cv2.cvtColor(img_right_rectified, cv2.COLOR_BGR2GRAY)
+
+num = 10   # numDisparities（视差范围数量）
+blockSize = 20 # blockSize（块大小）
+
+#SGBM算法算出的视差值会乘以16
+stereo = cv2.StereoSGBM_create(minDisparity=0, numDisparities=16 * num, blockSize=blockSize)
+disparity = stereo.compute(imgL, imgR)
+
+dis_color = disparity
+dis_color = cv2.normalize(dis_color, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype = cv2.CV_8U)
+dis_color = cv2.applyColorMap(dis_color, 2)
+cv2.imshow("depth", dis_color)
+cv2.waitKey(0)
+cv2.destroyWindow("depth")
+
+b= 11.97  # 相机之间的基线距离
+f = 1910  # 相机的焦距
+cx, cy = 1138, 651   # 主点
+
+i = 0
+output_points = np.zeros((2208 * 1242, 6))
+for row in range(disparity.shape[0] - 1):
+    for col in range(disparity.shape[1] - 1):
+        dis = disparity[row][col]
+        if dis != 0 and dis != (-16) and dis > 1200 and dis < 1700:
+            if i < len(output_points):
+                output_points[i][0] = 16*b*(col-cx)/dis
+                output_points[i][1] = 16*b*(row-cy)/dis
+                output_points[i][2] = 16*b*f/dis
+                output_points[i][3] = img_color[row][col][0]
+                output_points[i][4] = img_color[row][col][1]
+                output_points[i][5] = img_color[row][col][2]
+                i += 1
+            else:
+                print(f"Warning: Trying to access index {i}, which is out of bounds.")
+    # 根据需要处理数组越界的情况，例如通过中断循环或其他方式
+output_points = output_points[:i]  # 仅保存非零点
+
+
+def create_output(vertices, filename):
+    ply_header = '''ply
+format ascii 1.0
+element vertex %(vert_num)d
+property float x
+property float y
+property float z
+property uchar red
+property uchar green
+property uchar blue
+end_header
+'''
+    with open(filename,'w') as file:
+        file.write(ply_header%dict(vert_num = len(vertices)))
+        np.savetxt(file, vertices, '%f %f %f %d %d %d')
+
+output_file = 'dc.ply'
+create_output(output_points, output_file)
+pcd = o3d.io.read_point_cloud(output_file)
+o3d.visualization.draw_geometries([pcd])
+cv2.waitKey(0)
+
+
+ ### ######               
+
+# with open('point_cloud2.txt', 'w') as file:
+#     for point in output_points[:i]:  # 假设 i 是有效数据的数量
+#         file.write(f"{point[0]} {point[1]} {point[2]} {point[3]} {point[4]} {point[5]}\n")
+# try:
+#     points = np.loadtxt('point_cloud2.txt')
+#     if points.size > 0:
+#         # 创建 Open3D 点云对象
+#         pcd = o3d.geometry.PointCloud()
+#         # 设置点云的坐标
+#         pcd.points = o3d.utility.Vector3dVector(points[:, :3])
+#         # 设置点云的颜色
+#         pcd.colors = o3d.utility.Vector3dVector(points[:, 3:6] / 255.0)
+#         # 可视化点云
+#         o3d.visualization.draw_geometries([pcd])
+#     else:
+#         print("点云文件为空，无法加载点云。")
+# except Exception as e:
+#     print(f"加载点云文件时出错: {e}")
